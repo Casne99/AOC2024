e7bda1281795b2cc1afb150fdd7d0d36494593aa
Casne99 <luca.casnedi@protonmail.com>
2024-12-03 22:02:12 +0100

    Minor changes again

--- src/3/part2.groovy
+++ src/3/part2.groovy
@@ -6,8 +6,8 @@ def setMode = [
         "don't()": { enabled = false }
 ]
 
-def ans = (lines.join() =~ /mul\((\d+),(\d+)\)|do\(\)|don't\(\)/).collect { match ->
-    setMode[match[0]]?.call()
+def ans = (lines.join() =~ /mul\((\d+),(\d+)\)|do(?:n't)?\(\)/).collect { match ->
+    setMode.getOrDefault(match[0], { -> }).call()
     enabled && match[1] ? (match[1] as int) * (match[2] as int) : 0
 }.sum()
 
