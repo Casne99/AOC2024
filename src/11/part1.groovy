eca0b29f61ecbe8c1c5b922ee8d932a098e0710c
Casne99 <luca.casnedi@protonmail.com>
2024-12-12 20:17:18 +0100

    Added day11 part1 solution

---
+++ src/11/part1.groovy
@@ -0,0 +1,21 @@
+def input = new File('in.txt').readLines().get(0).split(' ').collect { it -> it as int }
+
+25.times {
+    input = input.collect { a -> step(a) }.flatten()
+            .collect { it as long}
+}
+
+println(input.size())
+
+
+static List<Long> step(final long input) {
+    if (input == 0)
+        return [1]
+
+    int middleIndex = "$input".length() / 2
+
+    if ("$input".length() % 2 == 0)
+        return ["$input".substring(0, middleIndex) as long, "$input".substring(middleIndex) as long]
+
+    return [input * 2024]
+}
